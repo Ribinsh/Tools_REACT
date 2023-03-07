b3dd0f6a1c4eff42c907454ab75c30b5707455d1
Ribinsh <ribinshp7@gmail.com>
2023-03-07 15:52:15 +0530

    axios

--- src/axios.jsx
+++ src/axios.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    // baseURL:'https://toool.arcticstore.ml/'
+    baseURL:'https://toool.arcticstore.ml/'
 
-    baseURL:'http://localhost:3000/'
+    // baseURL:'http://localhost:3000/'
 })
 
 export default instance
